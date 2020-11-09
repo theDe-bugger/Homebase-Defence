252953d06ec650a2748a6dd2817247d1f112254c
HarshS19 <71536091+HarshS19@users.noreply.github.com>
2020-11-09 12:58:25 -0500

    Deleted unnecessary comments

--- StartScreen.java
+++ StartScreen.java
@@ -31,63 +31,54 @@ public class StartScreen extends javax.swing.JFrame {
         // jPanel1.setSize(715,395);
         // jPanel1.setResizable(false);
 
-        // creating a label to display the title of the screen
         title.setFont(new java.awt.Font("Helvetica Neue", 0, 36)); // NOI18N
         title.setForeground(new java.awt.Color(255, 255, 255));
         title.setText("Homebase Defence");
         jPanel1.add(title);
         title.setBounds(210, 0, 350, 70);
 
-        // creating a label to display subtitle for how to play section
         instruct.setFont(new java.awt.Font("Helvetica Neue", 0, 26)); // NOI18N
         instruct.setForeground(new java.awt.Color(255, 255, 255));
         instruct.setText("How to Play?");
         jPanel1.add(instruct);
         instruct.setBounds(20, 70, 160, 30);
 
-        // creating a label to display the goal of the game
         goal.setFont(new java.awt.Font("Helvetica Neue", 0, 14)); // NOI18N
         goal.setForeground(new java.awt.Color(255, 255, 255));
         goal.setText("* The goal of the game is to destroy as many asteroids as possible to defend your Homebase!");
         jPanel1.add(goal);
         goal.setBounds(20, 120, 670, 20);
 
-        // creating a label to display instructions
         match.setFont(new java.awt.Font("Helvetica Neue", 0, 14)); // NOI18N
         match.setForeground(new java.awt.Color(255, 255, 255));
         match.setText("* Match the pattern at the bottom of the screen to destory an asteroid.");
         jPanel1.add(match);
         match.setBounds(20, 130, 670, 60);
-
-        // creating a label to display instructions
+        
         points.setFont(new java.awt.Font("Helvetica Neue", 0, 14)); // NOI18N
         points.setForeground(new java.awt.Color(255, 255, 255));
         points.setText("* Each broken asteroid is worth 5 points. In the next round, each asteroid destroyed is worth 5 more points.");
         jPanel1.add(points);
         points.setBounds(20, 160, 700, 60);
 
-        // creating a label to display instructions
         notMatched.setFont(new java.awt.Font("Helvetica Neue", 0, 14)); // NOI18N
         notMatched.setForeground(new java.awt.Color(255, 255, 255));
         notMatched.setText("* If you fail to match the pattern, asteroids move one step closer to your Homebase.");
         jPanel1.add(notMatched);
         notMatched.setBounds(20, 190, 670, 60);
 
-        // creating a label to display instructions
         health.setFont(new java.awt.Font("Helvetica Neue", 0, 14)); // NOI18N
         health.setForeground(new java.awt.Color(255, 255, 255));
         health.setText("* Each time an asteroid hits your Homebase, you will lose health.");
         jPanel1.add(health);
         health.setBounds(20, 220, 670, 60);
 
-        // creating a label to display instructions
         gameOver.setFont(new java.awt.Font("Helvetica Neue", 0, 14)); // NOI18N
         gameOver.setForeground(new java.awt.Color(255, 255, 255));
         gameOver.setText("* If you have no health remaining, the game is over.");
         jPanel1.add(gameOver);
         gameOver.setBounds(20, 250, 670, 60);
 
-        // creating a label to display group name and people involved
         groupName.setFont(new java.awt.Font("Helvetica Neue", 0, 14)); // NOI18N
         groupName.setForeground(new java.awt.Color(255, 255, 255));
         groupName.setText("GROUP 4 - Jaimil, Sam, Harsh, Vinay, and Jainil");
